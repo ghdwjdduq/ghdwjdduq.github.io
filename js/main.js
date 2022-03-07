bcbc8cccf26bc2fb16d526a6facb33c5cd385a6d
Jeongyeob Hong <aether0211@gmail.com>
2022-03-06 21:53:32 -0600

    Removing Loader

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+    "use strict"
+
+    //remove loader //
+    $('.loader').css('opacity',0);
+    setTimeout(function(){$('.loader').hide();}, 600)
+
+    // Fix Nav Bar //
+    $(window).scroll(function(){
+		if($(window).scrollTop() > 500 && !$('.mobile-toggle').is(":visible")){
+			$('.top-bar').addClass('nav-sticky');
+		}else{
+			$('.top-bar').removeClass('nav-sticky');
+		}
+	});
+
+
+})

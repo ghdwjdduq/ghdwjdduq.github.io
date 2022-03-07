ce831063086b80a38ff98f2b067c7bf018d5988e
Jeongyeob Hong <aether0211@gmail.com>
2022-03-06 22:27:29 -0600

    Loader

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
 $(document).ready(function(){
     "use strict"
-
-    //remove loader //
-    $('.loader').css('opacity', 0);
-    setTimeout(function(){$('.loader').hide();}, 600);
-
     // Fix Nav Bar //
     $(window).scroll(function(){
 		if($(window).scrollTop() > 500 && !$('.mobile-toggle').is(":visible")){
@@ -15,4 +10,44 @@ $(document).ready(function(){
 	});
 
 
-})
+});
+
+$(window).load(function(){
+
+    "use strict"
+
+    // Align vertically
+    alignVertical();
+    alignBottom();
+
+    $(window).resize(function(){
+        alignVertical();
+        alignBottom();
+    });
+
+    //remove loader //
+    $('.loader').css('opacity', 0);
+    setTimeout(function(){$('.loader').hide();}, 600);
+});
+
+function alignVertical(){
+
+    $('.align-vertical').each(function(){
+        var that = $(this);
+        var height = that.height();
+        var parentHeight = that.parent().height();
+        var padAmount = (parentHeight / 2) - (height/2);
+        that.css('padding-top', padAmount);
+    });
+
+}
+
+function alignBottom(){
+$('.align-bottom').each(function(){
+    var that = $(this);
+    var height = that.height();
+    var parentHeight = that.parent().height();
+    var padAmount = (parentHeight) - (height) - 32;
+    that.css('padding-top', padAmount);
+});
+}

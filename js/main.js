4e38dfbfad56fde1c07f13dbfb9c6385c120765b
Jeongyeob Hong <aether0211@gmail.com>
2022-03-06 22:02:11 -0600

    Really really solved spinner

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     "use strict"
 
     //remove loader //
-    $('.spinner').css('opacity',0);
-    setTimeout(function(){$('.spinner').hide();}, 600)
+    $('.loader').css('opacity', 0);
+    setTimeout(function(){$('.loader').hide();}, 600)
 
     // Fix Nav Bar //
     $(window).scroll(function(){

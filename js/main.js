14dcc0de821cb0d822609d10641e0d30600959ac
Jeongyeob Hong <aether0211@gmail.com>
2022-03-06 21:59:38 -0600

    Removing spinner

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     "use strict"
 
     //remove loader //
-    $('.loader').css('opacity',0);
-    setTimeout(function(){$('.loader').hide();}, 600)
+    $('.spinner').css('opacity',0);
+    setTimeout(function(){$('.spinner').hide();}, 600)
 
     // Fix Nav Bar //
     $(window).scroll(function(){
